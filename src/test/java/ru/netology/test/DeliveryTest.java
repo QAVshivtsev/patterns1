d6b3ecb2e428fe396de546920d0836c79a7d50a5
QAVshivtsev <159697104+QAVshivtsev@users.noreply.github.com>
2024-06-12 14:16:51 +0300

    Change Test

--- src/test/java/ru/netology/test/DeliveryTest.java
+++ src/test/java/ru/netology/test/DeliveryTest.java
@@ -1,17 +1,14 @@
 package ru.netology.test;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 import ru.netology.data.DataGenerator;
-
 import java.time.Duration;
-
-import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 
@@ -35,31 +32,34 @@ class DeliveryTest {
         String phone = DataGenerator.generatePhone("ru");
 
         Configuration.holdBrowserOpen = true;
-        $("span[data-test-id = city]").click();
+
         $("input[placeholder=Город]").setValue(city);
-        $("span[class=menu-item__control]").click();
+
         $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.DELETE);
         $x("//input[@placeholder='Дата встречи']").setValue(firstMeetingDate);
         $("input[name=name]").setValue(name);
-        $("span[data-test-id=phone]").click();
+
         $("input[name=phone]").setValue(phone);
         $("label[data-test-id=agreement]").click();
         $(byText("Запланировать")).click();
-        $(withText("Успешно!")).should(appear, Duration.ofSeconds(15));
-        $x("//div[contains(text(), 'Успешно!')]");
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно запланирована на " + firstMeetingDate));
         refresh();
-        $("span[data-test-id = city]").click();
+
         $("input[placeholder=Город]").setValue(city);
-        $("span[class=menu-item__control]").click();
+
         $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.DELETE);
-        $x("//input[@placeholder='Дата встречи']").setValue(firstMeetingDate);
+        $x("//input[@placeholder='Дата встречи']").setValue(secondMeetingDate);
         $("input[name=name]").setValue(name);
-        $("span[data-test-id=phone]").click();
+
         $("input[name=phone]").setValue(phone);
         $("label[data-test-id=agreement]").click();
         $(byText("Запланировать")).click();
         $x("//*[contains(text(), 'У вас уже запланирована встреча на другую дату. Перепланировать?!");
         $x("//span[contains(text(), 'Перепланировать')]").click();
-        $x("//div[contains(text(), 'Успешно!')]");
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно запланирована на " + secondMeetingDate));
     }
 }
